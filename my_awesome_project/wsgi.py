375617c4685abe759ec859b081f5e21a17277e84
SafwanSayeem <safwansayeem@gmail.com>
2025-04-19 18:27:50 +0600

    eeeeeeeee
    fefffffffff

--- my_awesome_project/wsgi.py
+++ my_awesome_project/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_awesome_project.settings')
+from whitenoise import WhiteNoise
 
-application = get_wsgi_application()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_awesome_project.settings')
+application = WhiteNoise(get_wsgi_application())
